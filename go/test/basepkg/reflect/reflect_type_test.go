45bb12ea637f4a684c510bcbc946d8d5fd809c8f
ykk <yukaokao301@mys4s.cn>
2018-08-09 17:37:35 +0800

    mdo

--- go/test/basepkg/reflect/reflect_type_test.go
+++ go/test/basepkg/reflect/reflect_type_test.go
@@ -21,3 +21,26 @@ func TestRefType(t *testing.T) {
 
 	assert.Equal(t, v.Type().String(), "basePkgT.Time")
 }
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func (u User) SetName(name string) {
+	u.Name = name
+}
+
+func (u *User) SetAge(age int) {
+	u.Age = age
+}
+
+func TestMtd(t *testing.T) {
+	u1 := User{Name: "user1", Age: 20}
+	u2 := &User{Name: "user2", Age: 21}
+
+	u1.SetName("u1")
+	u2.SetName("u2")
+	assert.Equal(t, u1.Name, "user1")
+	assert.Equal(t, u2.Name, "u2")
+}
